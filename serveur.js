cc4525a68a8684763cbe3c6a13adc8ac287a3bb9
Xamora64 <49618907+Xamora64@users.noreply.github.com>
2022-07-04 20:08:32 +0200

    Test

--- serveur.js
+++ serveur.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/PorteFolioXamora"));
+app.use(express.static(__dirname + "/dist/porte-folio-xamora"));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/PorteFolioXamora/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/porte-folio-xamora/index.html'));
 })
 
 app.listen(process.env.PORT);
